e0842438741332f150642d87b15965b078f39ad3
Jeff <jeffrey.kloos@gmail.com>
2021-01-22 19:58:56 +0100

    check for logs.

--- lumberjack.lua
+++ lumberjack.lua
@@ -1,19 +1,32 @@
+local BLOCK = "oak_log"
+
 local IDLE = 0
 local FELLING = 1
 local RETURNING = 2
 
-local function checkGrowth()
-    return turtle.inspect()
+local function Check()
+    local success, block = turtle.inspect()
+
+    if success then
+        return block.tags["minecraft:logs"]
+    else 
+        return false
+    end
 end
 
 local function Idle()
     
+
 end
 
 print("Well I'm a lumberjack and I'm OK! ^.^")
 local state = IDLE
 
-print(turtle.inspect())
+if Check() then 
+    print('It a log!')
+else
+    print('not log!')
+end
 
 --main loop
 --while true do
