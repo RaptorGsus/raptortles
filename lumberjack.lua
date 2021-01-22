6b205ca3112089b356415131ba1326b4bb1c9cca
Jeff <jeffrey.kloos@gmail.com>
2021-01-22 21:07:57 +0100

    Add Felling ability.

--- lumberjack.lua
+++ lumberjack.lua
@@ -1,41 +1,55 @@
-local BLOCK = "oak_log"
-
 local IDLE = 0
 local FELLING = 1
 local RETURNING = 2
 
-local function Check()
-    local success, block = turtle.inspect()
+local function Check(dir)
+    local success, block
+    
+    if dir == "up" then
+        success, block = turtle.inspectUp()
+    elseif dir == "down" then
+        success, block = turtle.inspectDown()
+    else
+        success, block = turtle.inspect()
+    end
 
     if success then
         return block.tags["minecraft:logs"]
-    else 
+    else
         return false
     end
 end
 
 local function Idle()
-    
+    if Check() then
+        print('Starting work! o7')
+        state = FELLING
+    end
+end
 
+local function Fell()
+    turtle.dig()
+    turtle.forward()
+
+    while Check("up") do
+        turtle.digUp()
+        turtle.up()
+    end
+
+    state = RETURNING
 end
 
 print("Well I'm a lumberjack and I'm OK! ^.^")
 local state = IDLE
 
-if Check() then 
-    print('It a log!')
-else
-    print('not log!')
-end
-
 --main loop
---while true do
-    
-  --  if      state == IDLE then Idle()
-
---end
-
-
+while true do
+    if      state == IDLE then Idle()
+    elseif  state == FELLING then Fell()
+        print("oops!")
+        break
+    end
+end
 
 
 
